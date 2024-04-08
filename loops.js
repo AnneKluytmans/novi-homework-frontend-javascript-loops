cc3b6d8cfb2c630399b71582c29fc1882f98238d
AnneKluytmans <annekluytmans1@gmail.com>
2024-04-08 11:45:43 +0200

    Finished loops.js

--- loops.js
+++ loops.js
@@ -11,8 +11,11 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
+console.log("\nOpdracht 1: \n");
 
-
+for (let i = 0; i < 5; i++) {
+    console.log("*".repeat(i + 1));
+}
 
 
 // ==========================================
@@ -25,8 +28,15 @@
 // klaar!
 // ==========================================
 
+console.log("\nOpdracht 2: \n");
 
-
+for (let i = 0; i < 5; i++) {
+    if (i === 4) {
+        console.log("klaar!");
+    } else {
+        console.log("loop...");
+    }
+}
 
 
 // ==========================================
@@ -45,9 +55,21 @@
 // >> 9
 // ==========================================
 
+console.log("\nOpdracht 3: \n");
 
+for (let i = 0; i < 10; i++) {
+    if (i < 3) {
+        console.log(i);
+    }
 
+    if (i >= 3 && i < 6) {
+        console.log('>', i);
+    }
 
+    if (i >= 6) {
+        console.log('>>', i);
+    }
+}
 
 // ==========================================
 // Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
@@ -87,5 +109,16 @@
 // etc.
 // ==========================================
 
+console.log("\nOpdracht 4: \n");
 
-
+for (let i =   1; i < 101; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
